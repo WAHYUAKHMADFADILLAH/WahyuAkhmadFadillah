e02c48e8c0ff4f258a89fe2889236cdd11677799
fadil's <fadil@Macbook.local>
2024-01-15 00:38:29 +0700

    ccoba aja

---
+++ java/main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class main {
+    public static void   {
+        int [] nama = {1,2,3,4,5};
+        System.out.println(nama);
+        System.out.println(Arrays.toString(nama));
+        System.out.println(nama[3]);
+            
+        }
+    }
